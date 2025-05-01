4ca1d67937b9c202b7dd4702894dd6ae2d77ba7d
RajeswariKilli <rajeswarikilli555@gmail.com>
2025-05-01 19:26:35 +0530

    completed

--- src/main/lab.py
+++ src/main/lab.py
@@ -1,3 +1,11 @@
+def count_vowels(string):
+    vowels = "aeiouAEIOU"
+    count = 0
+    for char in string:
+        if char in vowels:
+             count +=1
+    return count
+       
 # lab.py
 
 # Create a function by following these instructions
